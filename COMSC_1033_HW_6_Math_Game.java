43d8596de27bba65e6daa7efbd3e545de0f3c5f0
Michael Dobrinski <michael.git2015@gmail.com>
2015-09-29 19:34:55 -0500

    Complete the SWITCH construct and method to get result of each round.

--- COMSC_1033_HW_6_Math_Game.java
+++ COMSC_1033_HW_6_Math_Game.java
@@ -19,11 +19,12 @@
 import java.util.Scanner;
 
 public class COMSC_1033_HW_6_Math_Game {
+	public static int score=0;
+	public static int level=1;
+	public static Scanner inputAnswer = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int minNum = 0, maxNum = 0, level=1;
-		int i = 0, score=0;
-		boolean gameOver = false;
+		int num1 = 0, num2 = 0;
 
 		Scanner input = new Scanner(System.in);
 		System.out.print("\nPlease enter your first name: ");
@@ -38,22 +39,72 @@ public class COMSC_1033_HW_6_Math_Game {
 		"If the problem is answered incorrectly you will lose 5 points and "+
 		"the\ndifficulty level will be reduced.");
 
+
+
 		for(int round=1; round<=4; round++){
 			level = (level<1)? 1: level;	// Make sure level does not go below 1.
 
 			switch (level){
 				case 1:
+					num1 =  randomNumber(0, 9);
+					num2 =  randomNumber(0, 9);
+					roundResult(num1, num2, playerName, round);
+					break;
+
+				case 2:
+					num1 =  randomNumber(10, 99);
+					num2 =  randomNumber(10, 99);
+					roundResult(num1, num2, playerName, round);
+					break;
+
+				case 3:
+					num1 =  randomNumber(100, 999);
+					num2 =  randomNumber(100, 999);
+					roundResult(num1, num2, playerName, round);
+					break;
+				case 4:
+					num1 =  randomNumber(1000, 9999);
+					num2 =  randomNumber(1000, 9999);
+					roundResult(num1, num2, playerName, round);
+					break;
+				default:
 
 			}	// End of switch.
-
-
-
 		} // End of for loop.
 
 
 
+
 		input.close();
+		inputAnswer.close();
 	} // ************** End of MAIN method ****************************
+
+/**
+ * Method to calculate the result of the round of addition.
+ * Inputs: the two numbers for the problem, the player name, and round number
+ * Output: The method does not return a value but modifies the global variables
+ * score and level.
+ */
+
+	private static void roundResult(int num1, int num2, String playerName,
+			int round) {
+		int playerAnswer;
+		int correctAnswer;
+		printRoundHeader(score, round, level, playerName);
+		correctAnswer = num1 + num2;
+		System.out.printf("Answer the problem: %d + %d = ", num1, num2);
+		playerAnswer = getAnswer();
+		if (playerAnswer == correctAnswer){
+			score += 5;
+			level += 1;
+			System.out.print("CORRECT");
+		}
+		else{
+			score -= 5;
+			level -= 1;
+			System.out.print("INCORRECT");
+		}
+	} // End of roundResult
 
 	/**
 	 * Generate a random number between mn (minimum) and mx (maximum) inclusive.
@@ -73,11 +124,21 @@ public class COMSC_1033_HW_6_Math_Game {
 	 * Output: void
 	 */
 	public static void printRoundHeader(int s, int r, int lvl, String player){
-		System.out.printf("\n\n*************************************************"
-			+ "\n******************** Round %d ********************\n\n",r);
+		System.out.printf("\n\n***************************************************"
+			+ "****************\n***************************** Round %d *************"
+			+ "****************\n",r);
 		System.out.printf("\n%s your score is %d and you are at a dificulty"
-			+ " level of %d.\n\n", player, s, lvl);
+			+ " level of %d.\n", player, s, lvl);
 	} // End of printRoundHeader.
+	/**
+	 * Method to prompt the player for the answer and then return that answer.
+	 * Inputs: none
+	 * Output: integer
+	 */
+	public static int getAnswer(){
+		int result = inputAnswer.nextInt();
+		return result;
+	}
 
 
 
