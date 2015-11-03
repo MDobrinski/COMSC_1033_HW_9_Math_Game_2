408c0e4ff5803ff3f441b9216fef863d406a2b96
Michael Dobrinski <michael.git2015@gmail.com>
2015-11-03 10:14:19 -0600

    Changed the multiplier in the randomNumber method from 100000 to 10000000.

--- MyRandoms.java
+++ MyRandoms.java
@@ -15,13 +15,14 @@ public class MyRandoms {
 	 * Output: random integer between mn and mx inclusive.
 	 */
 	public static int randomNumber(int mn, int mx){
+
 		int temp;
 		if  (mn>mx){
 			temp=mn;
 			mn=mx;
 			mx=temp;
 		}
-		return ((mx - mn + 1) * (int)(Math.random()*100000)) / 100000 + mn;
+		return ((mx - mn + 1) * (int)(Math.random()*10000000)) / 10000000 + mn;
 	}
 // Return a random integer between 0 and 9 inclusive. 0<= random <=9
 	public static int get1DigRandom (){
